a8be2e5019b1d214a5b970af5130f0a622548996
Jason Wang <wangpiju0420@gmail.com>
2016-06-11 19:47:45 +0800

    select filtered data

--- src/main/scala/spark/example/Csv.scala
+++ src/main/scala/spark/example/Csv.scala
@@ -9,6 +9,7 @@ import org.apache.spark.rdd.RDD
 import scala.collection.mutable.Set
 /**
   * Created by phyrextsai on 2016/6/7.
+  * Edit by jesswang on 2016/06/07
   */
 
 
@@ -37,21 +38,23 @@ object Csv {
 
     val end = java.lang.System.currentTimeMillis()
     println("end : " + end)
+    sc.stop()
   }
 
   def showCSVData(csv: RDD[String], columnMap: Map[String, String]) : Unit = {
     val rows = csv.map(line => line.split(","))
-    var rowCount = 0;
+    var rowCount = 0
     for (row <- rows) {
-      rowCount += 1;
+      rowCount += 1
       if (rowCount > 1) {
-        var columnCount = 0;
+        var columnCount = 0
         for (data <- row) {
-          //println("data[" + columnCount + "] : " + data)
-          columnCount += 1;
+         // println("data[" + columnCount + "] : " + data)
+          columnCount += 1
           if (data != null && data.trim != "") {
             val key = columnCount + ""
             val column = columnMap.get(key).getOrElse(null)
+            //println(column)
             if (column != null) {
               //println("key : " + key + "," + column)
               columnSet += column
@@ -61,12 +64,13 @@ object Csv {
         }
       }
     }
+    println(columnSet.size)
   }
 
   def showCSVHeader(csv: RDD[String]) : Map[String, String] = {
     var columnMap :Map[String, String] = Map()
     val rows = csv.map(line => line.split(","))
-    var columnCount = 0;
+    var columnCount = 0
     val columns = rows.first()
     columns.foreach(column => {
       //println("column[" + columnCount + "] : " + column)
@@ -78,14 +82,19 @@ object Csv {
   }
 
   def queryCSV(sc: SparkContext): Unit = {
-    var sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
     val csvAll = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "true") // Use first line of all files as header
+      .option("inferScheme", "true")
       .load("Scorecard_small.csv")
 
-    var count = 0;
+    csvAll.registerTempTable("records")
+
+    var count = 0
     var str = ""
+
     columnSet.foreach((c) => {
       if(count > 0){
         str += " ," + c
@@ -95,11 +104,12 @@ object Csv {
       count += 1
     })
 
-    // TODO put column in the select
-    csvAll
-      .select("Id", "UNITID", "OPEID", "opeid6", "INSTNM", "CITY", "STABBR", "ZIP", "AccredAgency")
-      .foreach(
-        println
-      )
+    // put column in the select
+    val records = sqlContext.sql("SELECT "+str+" FROM records ")
+    records.foreach(
+      println
+    )
+
+
   }
 }
