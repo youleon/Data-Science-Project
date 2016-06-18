68bc448685993f2144611c211774ac5e027b6032
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-06-19 00:51:44 +0800

    src/main/scala/spark/example/mllib/RandomForestClassification.scala

--- src/main/scala/spark/example/mllib/RandomForestClassification.scala
+++ src/main/scala/spark/example/mllib/RandomForestClassification.scala
@@ -24,13 +24,13 @@ object RandomForestClassification {
 
     // Train a RandomForest model.
     // Empty categoricalFeaturesInfo indicates all features are continuous.
-    val numClasses = 10
-    val categoricalFeaturesInfo = Map[Int, Int]()
-    val numTrees = 1 // Use more in practice.
-    val featureSubsetStrategy = "all" // Number of features to consider for splits at each node. Supported: "auto", "all", "sqrt", "log2", "onethird". If "auto" is set, this parameter is set based on numTrees: if numTrees == 1, set to "all"; if numTrees > 1 (forest) set to "sqrt".
-    val impurity = "gini"
-    val maxDepth = 3
-    val maxBins = 100
+    val numClasses = 9
+    val categoricalFeaturesInfo = Map[Int, Int](/*0 -> 0, 1 -> 1, 2 -> 2, 5 -> 5, 8 -> 8, 13 -> 13, 15 -> 15, 19 -> 19, 30 -> 30, 37 -> 37, 58 -> 58, 59 -> 59*/) // Map storing arity of categorical features. E.g., an entry (n -> k) indicates that feature n is categorical with k categories indexed from 0: {0, 1, ..., k-1}.
+    val numTrees = 100 // Use more in practice.
+    val featureSubsetStrategy = "sqrt" // Number of features to consider for splits at each node. Supported: "auto", "all", "sqrt", "log2", "onethird". If "auto" is set, this parameter is set based on numTrees: if numTrees == 1, set to "all"; if numTrees > 1 (forest) set to "sqrt".
+    val impurity = "entropy" // Criterion used for information gain calculation. Supported values: "gini" (recommended) or "entropy".
+    val maxDepth = 4 // Maximum depth of the tree. E.g., depth 0 means 1 leaf node; depth 1 means 1 internal node + 2 leaf nodes. (suggested value: 4)
+    val maxBins = 10 // maximum number of bins used for splitting features (suggested value: 100)
 
 
     /*val model = RandomForest.trainClassifier(trainingData,
